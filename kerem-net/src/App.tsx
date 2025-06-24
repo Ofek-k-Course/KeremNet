7f54a87eec313b62ba1f4e1b9f964d1a4a608530
Ofek Tal <ofeki884@gmail.com>
2025-06-24 12:04:08 +0300

    created a KeremNet sideBar and added test buttons

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './Pages/HomePage/HomePage';
+import SideBar from './Components/applicationLayout/SideBar/SideBar';
 
 function App() {
   return (
+
     <div className="App">
+      <SideBar></SideBar>
       <HomePage></HomePage>
     </div>
+
   );
 }
 
