3f4d3568fff339ab22b566ec4a5688dde8176699
Ofek Tal <ofeki884@gmail.com>
2025-06-24 16:14:39 +0300

    created routes and controllers for keremNet's api

---
+++ server-side/src/controllers/postsController.ts
@@ -0,0 +1,31 @@
+import posts from "../assets/Posts.json";
+import express, { Request, Response } from "express";
+
+
+const getPosts = (req:Request, res:Response):void => {
+  res.send(posts);
+}
+
+const getPostsById = (req:Request, res:Response):void => {
+  const user = posts.find(post => post.id === req.params.id)
+  if (!user){
+    res.status(404).send('Post not found');
+    return
+  }
+  res.send(user);
+}
+
+const getPostAuthor = (req:Request, res:Response):void =>{
+  const user = posts.find(post => post.id === req.params.id)
+  if (!user){
+    res.status(404).send('Post not found');
+    return
+  }
+  res.send(user.author);
+}
+
+export default {
+    getPosts,
+    getPostsById,
+    getPostAuthor
+}
