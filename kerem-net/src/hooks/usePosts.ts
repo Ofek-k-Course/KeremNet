5b94a5a97cb7e4244181f5dd4c183155b0cd859d
Ofek Tal <ofeki884@gmail.com>
2025-06-24 12:04:08 +0300

    created a KeremNet sideBar and added test buttons

--- kerem-net/src/hooks/usePosts.ts
+++ kerem-net/src/hooks/usePosts.ts
@@ -9,7 +9,6 @@ export default function usePosts():postModel[]{
         
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             setPosts(data);});
     }, [])
     return Posts
