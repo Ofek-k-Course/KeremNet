ccbbdad30aef5d4e8f859473a74de9cc3aee3ab7
Ofek Tal <ofeki884@gmail.com>
2025-06-25 16:32:27 +0300

    added routing between pages and page switching in sidebar

--- kerem-net/src/Components/applicationLayout/SideBar/SideBar.tsx
+++ kerem-net/src/Components/applicationLayout/SideBar/SideBar.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './SideBar.css'
 import logo from '../../../Assets/logo.png'
 import { Button } from '@mui/material'
-
+import { useNavigate } from 'react-router-dom'
+import { BugReport, Home } from '@mui/icons-material'
 export default function SideBar() {
+  const navigate = useNavigate();
+
   return (
     <div className='sidebar-body'>
         <div className='sidebar-top'>
@@ -11,9 +14,19 @@ export default function SideBar() {
             <h1>KeremNet</h1>
         </div>
         <div className='sidebar-content'>
-            <Button className='sidebar-button' variant='outlined'>test</Button>
-            <Button className='sidebar-button' variant='outlined'>test</Button>
-            <Button className='sidebar-button' variant='outlined'>test</Button>
+            <Button 
+            className='sidebar-button'
+            variant='outlined' 
+            startIcon={<Home />}
+            onClick={()=>navigate("/")}
+            >Home</Button>
+            
+            <Button 
+            className='sidebar-button'
+            variant='outlined'
+            startIcon={<BugReport />}
+            onClick={()=>navigate("/test")}
+            >Test</Button>
         </div>
     </div>
   )
--- kerem-net/src/Pages/Pages.tsx
+++ kerem-net/src/Pages/Pages.tsx
@@ -1,11 +1,16 @@
 import React, { ReactElement } from 'react'
 import HomePage  from './HomePage/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 export default function page():ReactElement {
   return (
     <div>
-      <HomePage></HomePage>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
